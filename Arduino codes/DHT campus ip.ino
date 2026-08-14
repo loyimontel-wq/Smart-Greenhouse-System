b146d5a2da961617785108424af258319d3eff1e
loyimontel-wq <loyimontel@gmail.com>
2026-08-14 13:32:59 +0200

    Implement DHT sensor data transmission over WiFi
    Added WiFi and DHT sensor functionality to read temperature and humidity, then send data to a server.

--- Arduino codes/DHT campus ip.ino
+++ Arduino codes/DHT campus ip.ino
@@ -1,1 +1,43 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <DHT.h>
 
+#define DHTPIN 4
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+const char* ssid = "Esme";
+const char* password = "12345678";
+
+String serverName = "http://10.1.1.69:5000/update_dht";
+
+void setup() {
+  Serial.begin(115200);
+
+  dht.begin();
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) delay(500);
+}
+
+void loop() {
+
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  if (isnan(temperature) || isnan(humidity)) return;
+
+  String url = serverName +
+    "?temperature=" + String(temperature) +
+    "&humidity=" + String(humidity);
+
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(url);
+    http.GET();
+    http.end();
+  }
+
+  delay(2000);
+}
